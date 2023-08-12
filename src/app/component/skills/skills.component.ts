30ebd3cf3755d6b71a2667d5d663d86453fa99cc
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-08-12 18:30:37 +0200

    add my skills container

--- src/app/component/skills/skills.component.ts
+++ src/app/component/skills/skills.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit{
+  icons :String[] = [
+    'icon1.png', 
+    'icon2.png', 
+    'icon3.png', 
+    'icon4.png', 
+    'icon5.png',
+    'icon6.png',
+    'icon7.png',
+    'icon8.png',
+    'icon9.png',
+    'icon10.png'
+  ];
+
+  constructor() { }
+
+  ngOnInit() {}
+  
 
 }
