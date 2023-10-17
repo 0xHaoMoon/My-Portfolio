bb7d9ca20dd7afebb1fced4cd7ca94e5f81f0aaf
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-10-17 11:29:08 +0200

    fixe layout with new picture

--- src/app/mock-project.ts
+++ src/app/mock-project.ts
@@ -1,8 +1,19 @@
 import { Project } from './project';
 
 export const PROJECTS: Project[] = [
+
     {
         id: 1,
+        name: 'Simple-CRM',
+        subheadline: 'Angular | Typescript | Firebase | SCSS  | Material Design ',
+        desc: 'A CRM prototype that is designed with Material Design, based on Angular, and using examples CRUD with Firebase',
+        image: 'Join_Kanban_Template.png',
+        URL: 'https://hao-truong.developerakademie.net/El-Pollo-Loco-main/index.html',
+        gitHub: 'https://github.com/0xHaoMoon/simple-crm',
+    },
+
+    {
+        id: 2,
         name: 'Join',
         subheadline: 'JavaScript | HTML | CSS',
         desc: 'Task manager inspired by the Kanban system. Create and organize tasks using the drag and drop function, assign these to users, and use specific categories.',
@@ -12,13 +23,13 @@ export const PROJECTS: Project[] = [
     },
 
     {
-        id: 2,
-        name: 'El Pollo Loco',
-        subheadline: 'JavaScript | OOP | HTML | CSS ',
-        desc: 'Javascript-based and object-orientated jump & run game. Help Pepe to get his taco back!',
-        image: 'El_Pollo_Loco_template.png',
+        id: 3,
+        name: 'Retro Pokedex',
+        subheadline: 'JavaScript | REST-API | HTML | CSS ',
+        desc: 'This simple library, based on the PokéAPI, provides and catalogues Pokémon information in a retro design.',
+        image: 'Join_Kanban_Template.png',
         URL: 'https://hao-truong.developerakademie.net/El-Pollo-Loco-main/index.html',
-        gitHub: 'https://github.com/0xHaoMoon/El-Pollo-Loco',
+        gitHub: 'https://github.com/0xHaoMoon/Pokedex',
     },
 
 ];
